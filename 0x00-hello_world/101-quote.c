bdc58b73f10f17c55fadf2590818857f2f19eda0
Elksass5588 <qsasomar3@gmail.com>
2023-03-20 14:12:49 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - a c pro
+ * Return: 1 (scc)
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
