42ebb02a8a8fca7921cfbcd9a33ae311e87e7111
Elksass5588 <qsasomar3@gmail.com>
2023-03-20 12:22:26 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
  * main 
+ * 
+ * Return: 0 (sucsess)
  **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
