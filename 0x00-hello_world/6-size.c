85742df9fdca6d0304f3f9c90c637458b8389508
Elksass5588 <qsasomar3@gmail.com>
2023-03-20 13:06:47 -0700

    m

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - programe print 
+ * Return: 0sucsses
+ */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float f;
+
+print("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)",(unsigned long) sizeof(a),(unsigned long),sizeof(b)(unsigned long),sizeof(c)(unsigned long),sizeof(d),(unsigned long) sizeof(f));
+return (0);
+}
