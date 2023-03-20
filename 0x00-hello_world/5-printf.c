b99e099054900f9fe6d54d6eab648847d63b5ef8
Elksass5588 <qsasomar3@gmail.com>
2023-03-20 12:44:21 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - outstring with new line
+ * Return: 0 (sucsses)
+ **/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
